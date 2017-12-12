f1f4d804224a14cbb24f0b7dce42a87936e5d8aa
Luis Ternera <lternerah@gmail.com>
2017-12-12 00:17:05 -0500

    Add the function to store in the database the user contact info

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import webapp2
 import jinja2
 
+from google.appengine.ext import db
+
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
@@ -17,6 +19,13 @@ class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.write(*a, **kw)
 
+class User(db.Model):
+    name = db.StringProperty(required = True)
+    organization = db.StringProperty(required = False)
+    email = db.StringProperty(required = True)
+    telephone = db.StringProperty(required = False)
+    created = db.DateTimeProperty(auto_now_add = True)
+
 class Home(Handler):
     def get(self):
         self.render('landing_page.html')
@@ -24,6 +33,20 @@ class Home(Handler):
 class Register(Handler):
     def get(self):
         self.render('register_page.html')
+
+    def post(self):
+        name = self.request.get("name")
+        organization = self.request.get("organization")
+        email = self.request.get("email")
+        telephone = self.request.get("telephone")
+
+        if name and email:
+            user = User(name = name, organization = organization, email = email, telephone = telephone)
+            user.put()
+            self.render('register_page_send.html')
+        else:
+            self.render('register_page.html', name = name, organization = organization, email = email, telephone = telephone)
+
 
 app = webapp2.WSGIApplication([('/', Home),
                                ('/register', Register) 
